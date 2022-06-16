c9273a6242651428e061ff53911c4e241b27de97
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-06-16 13:54:17 +0530

    chore: add env template and deployment networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,6 +28,10 @@ module.exports = {
       url: process.env.ROPSTEN_URL || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
