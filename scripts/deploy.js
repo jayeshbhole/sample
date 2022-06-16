1ae4f152ac9eae2ea40583c98612c2d0cc2dfb7a
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-06-16 13:54:33 +0530

    feat: add hello world

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,17 +14,18 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  // Start deployment, returning a promise that resolves to a contract object
+  const hello_world = await HelloWorld.deploy("Hello World!");
+  console.log("Contract deployed to address:", hello_world.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
